80372799d571eaee34037db0d8e6cdaa3b87107e
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-02-26 03:28:52 +0530

    removed all the handlers from main to handlers file.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,39 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	_, _ = w.Write([]byte("Hello from SnippetBox..."))
-}
-
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "GET" {
-		w.Header().Set("Allow", "GET")
-		http.Error(w, "Method not allowed!", 405)
-		return
-	}
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-	_, _ = fmt.Fprintf(w, "A specific snippet of ID...%d", id)
-}
-
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method not allowed!", 405)
-		return
-	}
-	_, _ = w.Write([]byte("Creating a new snippet..."))
-}
 
 func main() {
 	mux := http.NewServeMux()
