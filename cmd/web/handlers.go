04ef5a162de7e0e364a7fd78be372e8a12e94dad
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-02-27 13:58:55 +0530

    replaced default error handling with custom err handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,35 +9,33 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
-	paths := []string{"ui/html/home.page.go.back",
+	paths := []string{"ui/html/home.page.go.html",
 		"ui/html/base.layout.go.html",
 		"ui/html/footer.partial.go.html",
 	}
 	ts, err := template.ParseFiles(paths...) // 'paths' is a variadic parameter
 	if err != nil {
-		app.logError.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
 		return
 	}
 	err = ts.Execute(w, nil)
 	if err != nil {
-		app.logError.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
 	}
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		w.Header().Set("Allow", "GET")
-		http.Error(w, "Method not allowed!", 405)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 	_, _ = fmt.Fprintf(w, "A specific snippet of ID...%d", id)
@@ -46,7 +44,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method not allowed!", 405)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	_, _ = w.Write([]byte("Creating a new snippet..."))
