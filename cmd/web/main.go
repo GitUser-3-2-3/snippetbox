2d67ecada0b18a8150c7fb6d557b0d20c4ac65ef
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-02-27 12:45:51 +0530

    added custom error logger to the http struct for our application

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,19 +7,12 @@ import (
 	"os"
 )
 
-func logInfo(msg string, val ...any) {
-	infoLog := log.New(os.Stdout, "INFO::\t", log.Ldate|log.Ltime)
-	infoLog.Printf(msg, val)
-}
-
-func logError(val ...any) {
-	errorLog := log.New(os.Stderr, "ERROR::\t", log.Ldate|log.Ltime|log.Lshortfile)
-	errorLog.Fatal(val)
-}
-
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address.")
 	flag.Parse()
+
+	logInfo := log.New(os.Stdout, "INFO::\t", log.Ldate|log.Ltime)
+	logError := log.New(os.Stderr, "ERROR::\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -29,7 +22,12 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	logInfo("Starting a server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	logError(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: logError,
+		Handler:  mux,
+	}
+	logInfo.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	logError.Fatal(err)
 }
