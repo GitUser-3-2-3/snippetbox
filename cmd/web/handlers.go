66a8939876431ec206d7643a50995ebb272b4f9a
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-02-27 13:09:13 +0530

    made the handler -> methods to application struct for consistent error handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,34 +3,33 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	paths := []string{"ui/html/home.page.go.html",
+	paths := []string{"ui/html/home.page.go.back",
 		"ui/html/base.layout.go.html",
 		"ui/html/footer.partial.go.html",
 	}
 	ts, err := template.ParseFiles(paths...) // 'paths' is a variadic parameter
 	if err != nil {
-		log.Println(err.Error())
+		app.logError.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.logError.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		w.Header().Set("Allow", "GET")
 		http.Error(w, "Method not allowed!", 405)
@@ -44,7 +43,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, "A specific snippet of ID...%d", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
 		http.Error(w, "Method not allowed!", 405)
