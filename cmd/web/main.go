71ea201aefc0cd5a1b0fca7e4d1755a2d9493f3a
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-02-27 21:12:04 +0530

    starts a connection pool with the database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -13,11 +16,18 @@ type application struct {
 }
 
 func main() {
+	dsn := flag.String("dsn", "web:Qwerty1,0*@/snippetbox?parseTime=true", "MySQL database.")
 	addr := flag.String("addr", ":4000", "HTTP network address.")
 	flag.Parse()
 
 	logInfo := log.New(os.Stdout, "INFO::\t", log.Ldate|log.Ltime)
 	logError := log.New(os.Stderr, "ERROR::\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		logError.Fatal(err)
+	}
+	_ = db.Close()
 
 	app := &application{
 		logError: logError,
@@ -30,6 +40,18 @@ func main() {
 		Handler:  app.routes(),
 	}
 	logInfo.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logError.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
