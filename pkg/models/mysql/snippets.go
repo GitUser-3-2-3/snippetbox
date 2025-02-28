d5efcc5fd1e507b3e652818b8a6a7d40cc6203a8
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-02-28 13:00:27 +0530

    func added to get the latest 10 values, and fixed returned err type.

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -3,6 +3,7 @@ package mysql
 import (
 	"database/sql"
 	"errors"
+	"log"
 
 	"snippetbox/pkg/models"
 )
@@ -36,7 +37,7 @@ func (mdl *SnippetModel) Get(id int) (*models.Snippet, error) {
 
 	switch {
 	case errors.Is(err, sql.ErrNoRows):
-		return nil, err
+		return nil, models.ErrNoRecord
 	case err != nil:
 		return nil, err
 	default:
@@ -45,5 +46,29 @@ func (mdl *SnippetModel) Get(id int) (*models.Snippet, error) {
 }
 
 func (mdl *SnippetModel) Latest() ([]*models.Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+			 WHERE expires > UTC_TIMESTAMP() ORDER BY created DESC LIMIT 10`
+
+	rows, err := mdl.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+	defer func(rows *sql.Rows) {
+		if err := rows.Close(); err != nil {
+			log.Printf("error closing rows: %v", err)
+		}
+	}(rows)
+	var snippets []*models.Snippet
+	for rows.Next() {
+		spt := &models.Snippet{}
+		err := rows.Scan(&spt.ID, &spt.Title, &spt.Content, &spt.Created, &spt.Expires)
+		if err != nil {
+			return nil, err
+		}
+		snippets = append(snippets, spt)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return snippets, nil
 }
