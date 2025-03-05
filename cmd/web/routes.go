315690d4879486353551026c7a3b2ecda3c72b83
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-06 02:58:49 +0530

    routes now uses httprouter to serve requests

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,19 +3,24 @@ package main
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
-func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
+func (bknd *backend) routes() http.Handler {
+	router := httprouter.New()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		bknd.notFound(w)
+	})
+	server := http.FileServer(http.Dir("./ui/static"))
+	router.Handler(http.MethodGet, "/static/*path", http.StripPrefix("/static", server))
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodGet, "/", bknd.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", bknd.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", bknd.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", bknd.snippetCreatePost)
 
-	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-	return standard.Then(mux)
+	standard := alice.New(bknd.recoverPanic, bknd.logRequest, secureHeaders)
+	return standard.Then(router)
 }
