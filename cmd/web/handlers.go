e5115ccad289471385a48881821ea4e5cb7e3470
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-05 16:08:33 +0530

    changed method names and how template data is injected

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -19,12 +19,12 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
-	app.render(w, r, "home.page.gohtml", &templateData{
-		Snippets: spt,
-	})
+	data := app.newTemplateData(r)
+	data.Snippets = spt
+	app.render(w, r, http.StatusOK, "home.gohtml", data)
 }
 
-func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
@@ -38,12 +38,12 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
-	app.render(w, r, "show.page.gohtml", &templateData{
-		Snippet: spt,
-	})
+	data := app.newTemplateData(r)
+	data.Snippet = spt
+	app.render(w, r, http.StatusOK, "view.gohtml", data)
 }
 
-func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", "POST")
 		app.clientError(w, http.StatusMethodNotAllowed)
