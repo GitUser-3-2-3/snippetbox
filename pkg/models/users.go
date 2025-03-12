f41018cb34df54af28452859f05af21c12dd91d4
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-12 21:43:04 +0530

    complete insert method for user creation

--- pkg/models/users.go
+++ pkg/models/users.go
@@ -2,7 +2,12 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"strings"
 	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Users struct {
@@ -18,13 +23,30 @@ type UserModel struct {
 }
 
 func (mdl *UserModel) Insert(name, email, password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+               VALUES (?, ?, ?, UTC_TIMESTAMP())`
+
+	_, err = mdl.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		var mysqlErr *mysql.MySQLError
+		if errors.As(err, &mysqlErr) && strings.Contains(mysqlErr.Message, "users_uc_email") {
+			if mysqlErr.Number == 1062 {
+				return ErrDuplicateEmail
+			}
+		}
+		return err
+	}
 	return nil
 }
 
-func (mdl *UserModel) Exists(id int) (bool, error) {
+func (mdl *UserModel) Exists(_ int) (bool, error) {
 	return false, nil
 }
 
-func (mdl *UserModel) Authenticate(email, password string) (int, error) {
+func (mdl *UserModel) Authenticate(_, _ string) (int, error) {
 	return 0, nil
 }
