1956c9c94d17d6225383576dd4d72e9d17e06e74
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-13 18:11:30 +0530

    inserted the authenticate middleware in the chain

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
+	"snippetbox/ui"
 )
 
 func (bknd *backend) routes() http.Handler {
@@ -13,10 +14,10 @@ func (bknd *backend) routes() http.Handler {
 	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
 		bknd.notFound(w)
 	})
-	server := http.FileServer(http.Dir("./ui/static"))
-	router.Handler(http.MethodGet, "/static/*path", http.StripPrefix("/static", server))
+	fileServer := http.FileServer(http.FS(ui.Files))
+	router.Handler(http.MethodGet, "/static/*path", fileServer)
 
-	dynamic := alice.New(bknd.sessionManager.LoadAndSave, noSurf)
+	dynamic := alice.New(bknd.sessionManager.LoadAndSave, noSurf, bknd.authenticate)
 
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(bknd.home))
 	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(bknd.snippetView))
