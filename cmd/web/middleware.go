342b1fbc4f8afd6200cfa6f0d4875483ccc45029
Parth Srivastav <parthsrivastav.00@gmail.com>
2025-03-13 16:46:58 +0530

    middleware added to set csrf protection cookies

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/justinas/nosurf"
 )
 
 func secureHeaders(next http.Handler) http.Handler {
@@ -43,4 +45,14 @@ func (bknd *backend) requireAuthentication(next http.Handler) http.Handler {
 		w.Header().Add("Cache-Control", "no-cache, no-store, must-revalidate")
 		next.ServeHTTP(w, r)
 	})
+}
+
+func noSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   true,
+	})
+	return csrfHandler
 }
